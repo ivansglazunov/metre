9ab2daf89d9eceb1dd2a17d1cbdde147a2d53801
menzorg <menzorg@gmail.com>
2018-06-25 07:55:43 +0400

    path fix + architect

--- components/routes.tsx
+++ components/routes.tsx
@@ -2,14 +2,11 @@ import * as React from 'react';
 
 import { Route } from 'react-router';
 
-import Header from './header';
-import About from './about';
+import About from './pages/about';
 
 export class Routes extends React.Component {
   render() {
-    return <div>
-      <Header/>
-      
+    return <div>      
       <Route path="/about" component={About} />
     </div>;
   }
