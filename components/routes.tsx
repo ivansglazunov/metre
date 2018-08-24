28c75a14aa63adc9873136641b655d9a5b70a623
romankrebs <41849774+romankrebs@users.noreply.github.com>
2018-08-24 21:09:30 +0300

    Удаление экспериментальной страницы

--- components/routes.tsx
+++ components/routes.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react';
 import { Switch, Route } from 'react-router';
 import About from './pages/about';
-import Maker from './pages/imagemaker';
-
 
 export class Routes extends React.Component {
   render() {
     return <div>
       <Switch>
         <Route path="/about" component={About} />
-        <Route path="/imagemaker" component={Maker} />
         <Route component={About} />
       </Switch>
     </div>;
