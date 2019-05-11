1843efd27fa03ded3e9bc24bf6a520540d6a97e5
Ivan S Glazunov <ivansglazunov@gmail.com>
2019-05-11 21:36:51 +0300

    fix field

--- imports/react/components/field.tsx
+++ imports/react/components/field.tsx
@@ -6,26 +6,38 @@ import {FormControl, InputLabel, OutlinedInput, MenuItem} from "@material-ui/cor
 // TODO check billing for edit pointer reset bug
 export class Field extends React.Component<any, any, any> {
   state = {
+    isFocused: false,
     value: this.props.value,
   };
-  componentWillReceiveProps(nextProps) {
-    if (this.props.value === this.state.value && nextProps.value !== this.props.value) {
-      this.setState({ value: nextProps.value });
-    }
-  }
-  onChange = (e) => {
+
+  handleChange = (e) => {
     this.setState({ value: e.target.value });
-    if (typeof(this.props.onChange) === 'function') {
-      this.props.onChange(e);
-    }
-  };
+    if (this.props.onChange) this.props.onChange(e);
+  }
+
+  handleFocus = (e) => {
+    this.setState({ isFocused: true });
+    if (this.props.onFocus) this.props.onFocus(e);
+  }
+
+  handleBlur = (e) => {
+    this.setState({ isFocused: false });
+    if (this.props.onBlur) this.props.onBlur(e);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!this.state.isFocused) this.setState({ value: nextProps.value });
+  }
+
   render() {
     const { style, ...props } = this.props;
     const { value } = this.state;
 
     return <TextField
       {...props}
-      onChange={this.onChange}
+      onChange={this.handleChange}
+      onFocus={this.handleFocus}
+      onBlur={this.handleBlur}
       value={value || ''}
       style={{margin: 0, padding: 0, ...style}}
       margin="normal"
