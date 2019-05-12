9bcb72db09b5f9483a4e20c108944a996a842982
Ivan S Glazunov <ivansglazunov@gmail.com>
2019-05-13 00:56:58 +0300

    dev/prod pages imports

--- imports/react/routes.tsx
+++ imports/react/routes.tsx
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import * as React from 'react';
 import { Switch, Route } from 'react-router';
 
@@ -6,37 +7,32 @@ import { getBundles } from 'react-loadable/webpack';
 
 import Nav from './components/nav';
 
-import NotFound from './pages/not-found';
-import NS from './pages/ns';
-import StateTable from './pages/state-table';
-import ParamsTable from './pages/params-table';
-import PropsTable from './pages/props-table';
-import Math from './pages/math';
+const isP = Meteor.isProduction;
 
-// const NotFound = Loadable({
-//   loader: () => import('./pages/not-found'),
-//   loading: () => <div></div>,
-// });
-// const NS = Loadable({
-//   loader: () => import('./pages/ns'),
-//   loading: () => <div></div>,
-// });
-// const StateTable = Loadable({
-//   loader: () => import('./pages/state-table'),
-//   loading: () => <div></div>,
-// });
-// const ParamsTable = Loadable({
-//   loader: () => import('./pages/params-table'),
-//   loading: () => <div></div>,
-// });
-// const PropsTable = Loadable({
-//   loader: () => import('./pages/props-table'),
-//   loading: () => <div></div>,
-// });
-// const Math = Loadable({
-//   loader: () => import('./pages/math'),
-//   loading: () => <div></div>,
-// });
+const NotFound = isP ? Loadable({
+  loader: () => import('./pages/not-found'),
+  loading: () => <div></div>,
+}) : require('./pages/not-found').default;
+const NS = isP ? Loadable({
+  loader: () => import('./pages/ns'),
+  loading: () => <div></div>,
+}) : require('./pages/ns').default;
+const StateTable = isP ? Loadable({
+  loader: () => import('./pages/state-table'),
+  loading: () => <div></div>,
+}) : require('./pages/state-table').default;
+const ParamsTable = isP ? Loadable({
+  loader: () => import('./pages/params-table'),
+  loading: () => <div></div>,
+}) : require('./pages/params-table').default;
+const PropsTable = isP ? Loadable({
+  loader: () => import('./pages/props-table'),
+  loading: () => <div></div>,
+}) : require('./pages/props-table').default;
+const Math = isP ? Loadable({
+  loader: () => import('./pages/math'),
+  loading: () => <div></div>,
+}) : require('./pages/math').default;
 
 export class Routes extends React.Component {
   render() {
