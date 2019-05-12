bf566157dadb55c73aff4cfc1909f3aa6230fc9e
Ivan S Glazunov <ivansglazunov@gmail.com>
2019-05-12 03:54:28 +0300

    connect status

--- imports/react/components/nav.tsx
+++ imports/react/components/nav.tsx
@@ -3,9 +3,18 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Nav extends React.Component {
+  state = {
+    status: 'connecting',
+  };
+  componentDidMount() {
+    Tracker.autorun(() => {
+      this.setState({ status: Meteor.status().status });
+    });
+  }
   makeReset = () => Meteor.call('nodes.reset');
   render() {
     return <ul>
+      <li>{this.state.status}</li>
       <li><Link to='/ns'>ns</Link></li>
       <li><Link to='/table'>table</Link></li>
       <li><Link to='/math'>math</Link></li>
